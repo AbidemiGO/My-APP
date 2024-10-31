91b2fde86f79693ae889969f09b9a3f7843726ee
DELL PRO <goluwafemi2009@gmail.com>
2024-10-31 01:08:08 +0100

    Initial commit

---
+++ app1.py
@@ -0,0 +1,81 @@
+
+import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    user_action = input('Type add, show, edit, complete or exit: ')
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+        todo = user_action[4:] + '\n'
+
+        todos = functions.get_todos()
+
+        todos.append(todo)
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith('show'):
+
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f'{index + 1} . {item}'
+
+            print(row)
+
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+            new_todo = input('Enter a new_todo: ')
+
+            todos = functions.get_todos()
+
+            todos[number] = new_todo + '\n'
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print('Invalid value is entered')
+            continue
+
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            index = number - 1
+
+            todos = functions.get_todos()
+
+            todos.pop(index)
+            todo_to_remove = todos[index].strip('\n')
+
+            functions.write_todos(todos)
+
+            message = f'Todo {todo_to_remove} was removed '
+            print(message)
+        except IndexError:
+            print('You entered invalid number')
+            continue
+
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print('Command is not valid')
+
+
+print('bye!')
+
+
+
+
+
+
+
+
+
+
---
+++ functions.py
@@ -0,0 +1,76 @@
+
+
+
+'''def asterik_triangle_soln(n):
+    return n**2
+
+ans= asterik_triangle_soln(6)
+print(ans)
+
+fruits = ['banana', 'apple', 'orange']
+len_var = len(fruits)
+print(len_var)
+
+detail={'name':'Gbenga',
+ 'address': '8, prague, czech republic',
+ 'age': 20
+ }
+det_dict = detail['address']
+print(det_dict)'''
+
+
+'''def convert_temp(celcius):
+    kelvin = 273 + celcius
+    return kelvin
+
+kelvin_temp = convert_temp(25)
+print(kelvin_temp)
+
+
+number = [1,2,3,4,5,6,7,8,9,10]
+for n in number:
+    print(n)
+
+add_plus = 4 + 5
+print(add_plus)
+
+def factor_ial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+calc = factor_ial(5)
+print(calc)
+
+
+va_num = [2, 5, 8, 9]
+def squared_num(n):
+    return n**2
+
+m_val = list(map(squared_num, va_num))
+print(m_val)'''
+
+FILEPATH = 'todos1.txt'
+
+def get_todos(filepath = FILEPATH):
+    """ Read a text file and return to_do
+    items list
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+        return todos_local
+
+
+def write_todos( todos_org,filepath = FILEPATH):
+    """ Write to_do items list in the text file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_org)
+
+
+if __name__ == "__main__":
+    print('Hello')
+    print('Hi')
+
+
+
+
